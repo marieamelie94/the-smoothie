1c5b1d8f4e8156b944ff7a94c4d78c08808cb9b0
Marie Amelie Sandrock <msandrock@audencia.com>
2018-11-27 18:36:35 +0100

    user logout sign in and up links + add first name to registration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  #skip_before_action :authenticate_user!, only: [:show]
+
   def show
     render template: "pages/#{params[:page]}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/pages/home' => 'pages#home'
-  get '/pages/:page' => 'pages#show'
+  # get '/pages/:page' => 'pages#show'
   get '/preferences' => 'preferences#edit'
 
-  resources :users, only: [:show, :edit, :update]
+  # resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
