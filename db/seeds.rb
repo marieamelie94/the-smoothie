1fdca83c41fea075f8fcb82284e46ce683847cf2
Marie Amelie Sandrock <msandrock@audencia.com>
2018-11-28 14:47:30 +0100

    smoothie index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# likes = %w(Avocado Coconut Pineapple)
-# likes.each do |likes|
-#   Like.create!(name: likes)
-# end
-
-# likes = %w(Lemon Spinach Pineapple)
-# likes.each do |likes|
-#   Dislike.create!(name: likes)
-# end
-
-# likes = %w(Peanuts Nuts Gluten Soy Lactose Celery Mustard Seasame)
-# likes.each do |likes|
-#   Allergie.create!(name: likes)
-# end
-
 p "creating stuff ..."
+
+likes = %w(Avocado Coconut Pineapple)
+likes.each do |likes|
+  Like.create!(name: likes)
+end
+
+likes = %w(Lemon Spinach Pineapple)
+likes.each do |likes|
+  Dislike.create!(name: likes)
+end
+
+likes = %w(Peanuts Nuts Gluten Soy Lactose Celery Mustard Seasame)
+likes.each do |likes|
+  Allergie.create!(name: likes)
+end
+
 
 i1 = Ingredient.create!(name: "Avocado")
 i2 = Ingredient.create!(name: "Spinach")
@@ -42,5 +43,5 @@ d4 = Dose.create!(grams: 100, ingredient_id: i3.id, smoothie_id: s1.id)
 d5 = Dose.create!(grams: 15, ingredient_id: i4.id, smoothie_id: s1.id)
 d5 = Dose.create!(grams: 15, ingredient_id: i6.id, smoothie_id: s1.id)
 
-p "done !!!   ...."
+p "Seed done ....! Now you'd better work bitch ! <3"
 
