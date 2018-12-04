79b317824f5d3eea95485a50273932e3e1c002c3
Marie Amelie Sandrock <msandrock@audencia.com>
2018-12-04 23:45:57 +0100

    allergies, dislikes and likes are not working but in preferences form

--- app/controllers/allergies_controller.rb
+++ app/controllers/allergies_controller.rb
@@ -3,16 +3,41 @@ class AllergiesController < ApplicationController
     @allergy = Allergy.new
   end
 
+  def index
+    @allergies = Allergy.all
+  end
+
+  def show
+    @allergie = Allergy.find(params[id])
+  end
+
   def create
     @user = current_user
-    @allergy = Allergy.new(allergy_params)
-    @allergy.user = @user
-    @allergy.save!
+    allergy_params[:ingredient].each do |ingredient|
+      p ingredient
+      if ingredient == ""
+        p "empty 1 ingredient"
+      else
+        @ingredient = Ingredient.where(name: ingredient.to_sym)
+        p "empty 2 ingredient"
+        if @ingredient.nil?
+          p "empty 2 ingredient"
+        else
+          p "hhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+          p "Hello #{@ingredient.name}"
+          @user.allergies << @ingredient
+
+          # @allergy = Allergy.new(user: @user, ingredient: @ingredient)
+          # @allergy.save!
+        end
+      end
+    end
+    render 'preferences/edit'
   end
 
   private
 
   def allergy_params
-    params.require(:allergy).permit(:ingredient_id)
+    params.require(:allergy).permit(ingredient: [])
   end
 end
