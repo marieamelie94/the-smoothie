57ff12aed57038a5358532e6b09e2b19a63c0a78
Marie Amelie Sandrock <msandrock@audencia.com>
2018-12-05 13:10:33 +0100

    preferences form fixed, order recap styled and mobile optimised

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,23 +5,35 @@ class LikesController < ApplicationController
 
   def create
     @user = current_user
-    like_param.each do |ingredient|
-      next unless ingredient.empty?
-      @ingedient = Ingredient.where(name: ingredient)
-      next unless !@ingredient.nil?
-      @like = Like.new(user: @user, ingredient: @ingredient)
-      # @allergy.user = @user
-      if @like.save!
-        render preferences_path
+    likes_params[:ingredient].each do |ingredient|
+      p ingredient
+      if ingredient == ""
+        p "empty 1 ingredient"
       else
-        render preferences_path
+        @ingredient = Ingredient.where(name: ingredient.to_sym)
+        p "empty 2 ingredient"
+        if @ingredient.nil?
+          p "empty 2 ingredient"
+        else
+          p "hhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+          p "Hello #{@ingredient.name}"
+          # @user.allergies = @ingredient
+
+          @like = Like.new(user_id: @user.id, ingredient_id: @ingredient.ids[0])
+          if @like.save!
+            p "saved ************"
+          else
+            p "failed !!!!!!!!!!!!"
+          end
+        end
       end
     end
+    render 'preferences/edit'
   end
 
   private
 
-  def like_param
-    params.require(:like).permit(ingredients: [])
+  def likes_params
+    params.require(:like).permit(ingredient: [])
   end
 end
