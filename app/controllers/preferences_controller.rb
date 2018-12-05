39ff297dfcda7e7fc1a470bd2394b777ab2c735d
Evgeniya Kirpicheva <jenny_blz@yahoo.com>
2018-12-05 19:25:37 +0100

    one form for all implementation

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,5 +2,8 @@ class PreferencesController < ApplicationController
   def edit
     @user = current_user
     @allergies = Allergy.all
+    @allergy = Allergy.new
+    @dislike = Dislike.new
+    @like = Like.new
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,19 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = current_user
+      params[:user][:allergy][:ingredient].each_with_index do |ingredient, index|
+      Allergy.create!(ingredient: Ingredient.find_by(name: ingredient), user: current_user) if index > 0
+    end
 
-    if @user.update(user_params)
+    params[:user][:dislike][:ingredient].each_with_index do |ingredient, index|
+      Dislike.create!(ingredient: Ingredient.find_by(name: ingredient), user: current_user) if index > 0
+    end
+
+    params[:user][:like][:ingredient].each_with_index do |ingredient, index|
+      Like.create!(ingredient: Ingredient.find_by(name: ingredient), user: current_user) if index > 0
+    end
+
+    if current_user.update(user_params)
       redirect_to smoothies_choose_amount_path
     else
       redirect_to preferences_path
@@ -18,4 +28,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:age, :gender, :weight, :height, :goal)
   end
+
 end
